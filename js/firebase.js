46b80f468d5683c7efd9b3ee709402130866619a
NemCaBong <89348778+NemCaBong@users.noreply.github.com>
2023-04-13 11:40:33 +0700

    initial commit

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDYXR0Q5x4sU0ZQEvbkPCf0xOCWVNJWX_g",
+	authDomain: "fir-lms-51b32.firebaseapp.com",
+	projectId: "fir-lms-51b32",
+	storageBucket: "fir-lms-51b32.appspot.com",
+	messagingSenderId: "319460554517",
+	appId: "1:319460554517:web:541953db73425f091adb1c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
